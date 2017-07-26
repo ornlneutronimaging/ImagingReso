7e5175e1c4bb3c4271d7d9379777adbe443b39b4
JeanBilheux <bilheuxjm@ornl.gov>
2017-07-26 15:02:19 -0400

    Thickness input and unit test

--- ImagingReso/resonance.py
+++ ImagingReso/resonance.py
@@ -5,14 +5,20 @@ class Resonance(object):
     
     database = 'ENDF_VIII'
     
-    def __init__(self, elements=[]):
-        if not (elements == []):
-            elements = list(set(elements))
-            for _element in elements:
-                if not is_element_in_database(element=_element, database=self.database):
-                    raise IOError("Element {} can not be found in database!".format(_element))
-            self.elements = elements
+    def __init__(self, elements=[], thicknesses=[]):
+        if elements == []:
+            raise ValueError("You need to define at least one element!")
+
+        elements = list(set(elements))
+        for _element in elements:
+            if not is_element_in_database(element=_element, database=self.database):
+                raise IOError("Element {} can not be found in database!".format(_element))
             
+        if thicknesses == []:
+            raise ValueError("You need to define at least 1 thickness!")
+            
+        if not (len(thicknesses) == len(elements)):
+            raise ValueError("Thickness and Elements arrays must have the same size!")
     
-    
-    
+        self.elements = elements
+        self.thickness = thickness
--- tests/ImagingReso/resonance_test.py
+++ tests/ImagingReso/resonance_test.py
@@ -11,14 +11,16 @@ class TestInitialization(unittest.TestCase):
 
         # unique element
         _elements = ['Ag','Au','Hf']
-        _o_reso = Resonance(elements=_elements)
+        _thickness = [0.025, 0.025, 0.025]
+        _o_reso = Resonance(elements=_elements, thicknesses=_thickness)
         _elements_expected = _elements.sort()
         _elements_returned = _o_reso.elements.sort()
         self.assertEqual(_elements_expected, _elements_returned)
     
         # duplicate element
         _elements = ['Ag','Au','Hf', 'Ag']
-        _o_reso = Resonance(elements=_elements)
+        _thickness = [0.025, 0.025, 0.025]
+        _o_reso = Resonance(elements=_elements, thicknesses=_thickness)
         _elements_expected = list(set(_elements)).sort()
         _elements_returned = _o_reso.elements.sort()
         self.assertEqual(_elements_expected, _elements_returned)
@@ -28,3 +30,18 @@ class TestInitialization(unittest.TestCase):
         _elements = ['Unknown','Au','Hf', 'Ag']
         self.assertRaises(IOError, Resonance, elements=_elements)
 
+    def test_initialization_must_contain_at_least_one_element(self):
+        '''assert at least one element must be defined'''
+        self.assertRaises(ValueError, _o_reso = Resonance)
+        
+    def test_initialization_must_contain_at_least_one_thickness(self):
+        '''assert at least one thickness must be defined'''
+        _elements = ['Ag']
+        self.assertRaises(ValueError, Resonance, elements=_elements)
+        
+    def test_initialization_raises_error_if_elements_and_thickness_size_do_not_match(self):
+        '''assert ValueError is raised when elements and thickness do not have the same size'''
+        _elements = ['Ag','Au','Hf']
+        _thickness = [0.025, 0.025]
+        self.assertRaises(ValueError, Resonance, _elements, _thickness)
+        
