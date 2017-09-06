440db28110e3ac0c5bdcc4fa6ef42b0ef9d66e1f
JeanBilheux <bilheuxjm@ornl.gov>
2017-09-06 16:53:31 -0400

    changed version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read_file(filename):
 
 setup(
     name = "ImagingReso",
-    version = "1.5.6",
+    version = "1.5.8",
     author = "Yuxuan (Shawn) Zhang, Jean Bilheux",
     author_email = "zhangy6@ornl.gov, bilheuxjm@ornl.gov",
     packages = find_packages(exclude=['tests', 'notebooks']),
