56f45a35f561ded5347e6cd8b7283ff2b669e4f5
Yuxuan Zhang <zhangy6x@gmail.com>
2018-05-18 17:32:40 -0400

    new release

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read_file(filename):
 
 setup(
     name="ImagingReso",
-    version="1.6.8a",
+    version="1.6.9",
     author="Yuxuan (Shawn) Zhang, Jean Bilheux",
     author_email="zhangy6@ornl.gov, bilheuxjm@ornl.gov",
     packages=find_packages(exclude=['tests', 'notebooks']),
